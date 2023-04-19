ea7de4216727f24abd071136d02767a0a00cab3f
Àlex <alexm0u.13@gmail.com>
2023-04-19 13:35:57 +0200

    FEAT: model view controller

--- index.js
+++ index.js
@@ -1,49 +1,27 @@
-const express = require('express');
-const { User } = require('./models/index')
-const db = require('./db/db');
-require('dotenv').config()
+const express = require("express");
+const db = require("./db/db");
+require("dotenv").config();
+
+const userRoutes = require("./views/userRoutes");
+// const authRoutes = require("./views/authRoutes");
+// const appointmentRoutes = require("./views/appointmentRoutes");
 
 const app = express();
 
 app.use(express.json());
+app.use(userRoutes);
+// app.use(authRoutes);
+// app.use(appointmentRoutes);
 const PORT = process.env.PORT || 3000;
 
-// app.listen(PORT, () => {
-//     console.log(`Server is running on port ${PORT}`)
-// })
+app.get("/welcome", (req, res) => {
+  return res.send("Bienvenido a mi app");
+});
 
-app.get('/welcome', (req, res) => {
-    return res.send("Bienvenido a mi app")
-})
 
-app.post('/user', async (req, res) => {
-    try{
-        const {name, email, password } = req.body;
-        const newUser = {
-            name: name,
-            email: email,
-            password: password
-        }
-        const users = await User.create(newUser)
-        return res.json(users)
-    } catch (error) {
-        return res.status(500).send(error.message)
-    }
-})
 
-// app.get('/username', async(req, res)=> {
-//     const users = await User.findAll({
-//         attributes: ['fullName','email']} )
-//             return res.json(users);
-//     })
-
-app.get('/user', async(req, res)=> {
-    const users = await User.findAll();
-    return res.json(users);
-})
 
 db.then(() => {
-    //Starting server
-    app.listen(PORT, () => console.log("Server on port " + PORT));
-})
-    .catch((err) => console.log(err.message));   
+  //Starting server
+  app.listen(PORT, () => console.log("Server on port " + PORT));
+}).catch((err) => console.log(err.message)); 
