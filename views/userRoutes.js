079b8e10d9900d133fe75f726d7a2295de113ea8
Àlex <alexm0u.13@gmail.com>
2023-04-20 15:50:32 +0200

    FEAT: Starting with middlewares

--- views/userRoutes.js
+++ views/userRoutes.js
@@ -1,7 +1,9 @@
 const userController = require("../controllers/userController");
+const verifyToken = require("../middlewares/verifyToken");
 const router = require("express").Router();
 
 router.post("/user", userController.newUser);
-router.get("/user", userController.getUser);
+router.get("/user", userController.getAllUser);
+router.put("/user/update/",verifyToken, userController.updateUser,);
 
 module.exports = router;
