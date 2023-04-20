4c21af43f53ba07558883a653f7e578362797a87
Àlex <alexm0u.13@gmail.com>
2023-04-20 15:41:35 +0200

    FEAT: Encrypted password ok, Auth controller done

---
+++ controllers/authController.js
@@ -0,0 +1,42 @@
+const { User } = require("../models/index");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const authController = {};
+
+authController.login = async (req, res) => {
+    try {
+        const { email, password, } = req.body;
+        const user = await User.findOne(
+            {
+        where: {
+            email: email,
+        },
+        }
+        );
+        if (!user) {
+        return res.send("Wrong User");
+        }
+        const isMatch = bcrypt.compareSync(password, user.password);
+        if (!isMatch) {
+        return res.send("Wrong credentials");
+        }
+        const token = jwt.sign(
+        {
+            userId: user.id,
+            email: user.email,
+            roleId: user.role_id,
+            name: user.name,
+        },
+        "secreto",
+        { expiresIn: "2h" }
+        );
+        return res.json(token);
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: "Ups, something were wrong",
+            error: error.message
+        })
+    }
+}
+module.exports = authController;
